4402e997a62ec39b0097be8fbd7f6586106f0777
HauntedPearl <rs.hauntedpearl@gmail.com>
2026-05-06 12:52:02 +0700

    feat: add /about to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,6 +24,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "monthly",
       priority: 0.5,
     },
+    {
+      url: "https://deadyet.wtf/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
     ...personEntries,
   ];
 }
